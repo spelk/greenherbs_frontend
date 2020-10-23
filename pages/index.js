eae368b1e1e78cf098bc64289255f07a07778df2
Толик Т2 <tolikt2@Mac-mini-Tolik.local>
2020-10-23 19:37:10 +0300

    start config next env

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  console.log(process)
+
   return (
     <div className={styles.container}>
       <Head>
