5cc0ecf17eb93295cb0b5cdbca3c8f922a494775
Timofey-Mileiko <mill.timofey@gmail.com>
2020-10-29 20:29:07 +0200

    empty commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import getConfig from "next/config";
+
+const { publicRuntimeConfig } = getConfig();
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const client = new ApolloClient({
+    uri: publicRuntimeConfig.API_ENDPOINT,
+    cache: new InMemoryCache(),
+  });
+
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ pages/test.js
@@ -0,0 +1,11 @@
+export default function Test() {
+  
+    return (
+      <div>
+  
+          <title>Create Next App</title>
+          <link rel="icon" href="/favicon.ico" />
+  
+      </div>
+    )
+  }
