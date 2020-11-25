dcdcf049804595dcfbc780ac44e2815657c494d7
alxbuts <alxbuts@gmail.com>
2020-11-25 20:10:59 +0200

    yoast seo init

---
+++ pages/seo/index.js
@@ -0,0 +1,66 @@
+import { gql, useQuery } from "@apollo/client";
+import { initializeApollo } from "../../src/apollo";
+import Head from 'next/head';
+
+const GET_SEO = gql`
+  query getSeo {
+    pageBy(pageId: 2) {
+      seo {
+        canonical
+        cornerstone
+        focuskw
+        metaDesc
+        metaKeywords
+        metaRobotsNofollow
+        metaRobotsNoindex
+        opengraphAuthor
+        opengraphDescription
+        opengraphModifiedTime
+        opengraphPublishedTime
+        opengraphSiteName
+        opengraphPublisher
+        opengraphTitle
+        opengraphType
+        opengraphUrl
+        twitterDescription
+        title
+        twitterTitle
+        schema {
+          articleType
+          pageType
+        }
+      }
+    }
+  }
+`;
+
+function Test({ data }) {
+  return (
+    <div>
+      <Head>
+        <title>{data.title}</title>
+      </Head>
+      <div>
+        Here is the seo content for this page, <pre></pre>
+      </div>
+      {console.log(data)};
+    </div>
+  );
+}
+
+// This gets called on every request
+export async function getServerSideProps() {
+  const apolloClient = initializeApollo();
+
+  await apolloClient.query({
+    query: GET_SEO
+  })
+
+  console.log(apolloClient.cache.extract())
+  // Pass data to the page via props
+  return { props: { 
+    data: apolloClient.cache.extract()
+   } };
+}
+
+export default Test;
