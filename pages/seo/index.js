070593e492d7043cd7fc6090ebae2cec892a73f0
alxbuts <alxbuts@gmail.com>
2020-12-07 11:52:13 +0200

    initial seo setup

--- pages/seo/index.js
+++ pages/seo/index.js
@@ -7,25 +7,58 @@ const GET_SEO = gql`
   query getSeo {
     page(id: "2", idType: DATABASE_ID) {
       seo {
+        canonical
+        cornerstone
+        focuskw
+        metaDesc
         metaKeywords
+        metaRobotsNofollow
+        metaRobotsNoindex
+        opengraphAuthor
+        opengraphDescription
+        opengraphModifiedTime
+        opengraphPublishedTime
+        opengraphSiteName
+        opengraphPublisher
         opengraphTitle
+        opengraphType
+        opengraphUrl
+        twitterDescription
         title
+        twitterTitle
+        schema {
+          articleType
+          pageType
+        }
       }
     }
   }
 `;
 
 function Seo() {
-  const {data} = useQuery(GET_SEO)
+  const {data} = useQuery(GET_SEO);
 
+  const seo = data.page.seo;
+
+  console.log(data.page.seo);
   return (
     <div>
       <Head>
-        <title>title</title>
+  <title>{seo.title}</title>
+        {seo.metaDesc ? <meta name="description" content={seo.metaDesc} /> : null}
+        {seo.opengraphTitle ? <meta property="og:title" content={seo.opengraphTitle} /> : null}
+        {seo.opengraphType ? <meta property="og:type" content={seo.opengraphType} /> : null}
+        {seo.opengraphPublisher ? <meta property="og:publisher" content={seo.opengraphPublisher} /> : null}
+        {seo.opengraphSiteName ? <meta property="og:site_name" content={seo.opengraphSiteName} /> : null}
+
+        {seo.opengraphAuthor ? <meta property="og:author" content={seo.opengraphAuthor} /> : null}
+        {seo.opengraphDescription ? <meta property="og:description" content={seo.opengraphDescription} /> : null}
+        {seo.opengraphModifiedTime ? <meta property="og:modified_time" content={seo.opengraphModifiedTime} /> : null}
+        {seo.opengraphPublishedTime ? <meta property="og:published_time" content={seo.opengraphPublishedTime} /> : null}
+        {seo.opengraphUrl ? <meta property="og:url" content={seo.opengraphUrl} /> : null}
       </Head>
       <div>
         Here is the seo content for this page, <pre>{JSON.stringify(data)}</pre>
-        {console.log(data)}
         <Link href="/test">test</Link> 
       </div>
     </div>
