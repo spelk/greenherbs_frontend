53f3d2275afe35be8c51fac55df71d16979f59a8
alxbuts <alxbuts@gmail.com>
2020-12-10 23:51:56 +0200

    custom assetPrefix added (https://nextjs.org/docs/api-reference/next.config.js/cdn-support-with-asset-prefix)

--- next.config.js
+++ next.config.js
@@ -7,5 +7,6 @@ module.exports = {
   },
   publicRuntimeConfig: {
     API_ENDPOINT: process.env.ENV === 'dev' ? 'http://api.dev.greenherbs.local/wp/graphql' : 'https://api.dev.greenherbs.ru/wp/graphql'
-  }
+  },
+  assetPrefix: process.env.ENV === 'dev' ? '' : 'https://cdn.alxbuts.dev/GreenHerbs/',
 }
