76ff32edee8863b8a0b3abd83198cbf399baf707
alxbuts <alxbuts@gmail.com>
2020-12-18 19:46:21 +0200

    trying to fix 500 error

--- Components/Seo-for-seo-page.js
+++ Components/Seo-for-seo-page.js
@@ -45,52 +45,7 @@ function SeoForSeoPage() {
 
   
   return (
-    <div>
-      <Head>
-        <title>{seo.title}</title>
-        {seo.metaDesc ? (
-          <meta name="description" content={seo.metaDesc} />
-        ) : null}
-        {seo.opengraphTitle ? (
-          <meta property="og:title" content={seo.opengraphTitle} />
-        ) : null}
-        {seo.opengraphType ? (
-          <meta property="og:type" content={seo.opengraphType} />
-        ) : null}
-        {seo.opengraphPublisher ? (
-          <meta property="og:publisher" content={seo.opengraphPublisher} />
-        ) : null}
-        {seo.opengraphSiteName ? (
-          <meta property="og:site_name" content={seo.opengraphSiteName} />
-        ) : null}
-
-        {seo.opengraphAuthor ? (
-          <meta property="og:author" content={seo.opengraphAuthor} />
-        ) : null}
-        {seo.opengraphDescription ? (
-          <meta property="og:description" content={seo.opengraphDescription} />
-        ) : null}
-        {seo.opengraphModifiedTime ? (
-          <meta
-            property="og:modified_time"
-            content={seo.opengraphModifiedTime}
-          />
-        ) : null}
-        {seo.opengraphPublishedTime ? (
-          <meta
-            property="og:published_time"
-            content={seo.opengraphPublishedTime}
-          />
-        ) : null}
-        {seo.opengraphUrl ? (
-          <meta property="og:url" content={seo.opengraphUrl} />
-        ) : null}
-      </Head>
-      <div>
-        Here is the seo content for this page, <pre>{JSON.stringify(data)}</pre>
-        <Link href="/test">test</Link>
-      </div>
-    </div>
+    <div>Hello world</div>
   );
 }
 
