f9a109be2bf6ee76b834414b07be7254d9a63cdc
alxbuts <alxbuts@gmail.com>
2020-12-21 16:18:11 +0200

    added http to the api endpoint

--- next.config.js
+++ next.config.js
@@ -6,6 +6,6 @@ module.exports = {
     ENV: process.env.ENV
   },
   publicRuntimeConfig: {
-    API_ENDPOINT: process.env.ENV === 'dev' ? 'http://api.dev.greenherbs.local/wp/graphql' : 'api.dev.greenherbs.ru/wp/graphql'
+    API_ENDPOINT: process.env.ENV === 'dev' ? 'http://api.dev.greenherbs.local/wp/graphql' : 'http://api.dev.greenherbs.ru/wp/graphql'
   }
 }
