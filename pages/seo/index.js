a7a4649dd71b79f6b37a1ddc4c119d5933cb3006
alxbuts <alxbuts@gmail.com>
2020-12-21 18:21:35 +0200

    testing hardcoded meta tags

--- pages/seo/index.js
+++ pages/seo/index.js
@@ -44,18 +44,12 @@ function Seo() {
   return (
     <div>
       <Head>
-  <title>{seo.title}</title>
-        {seo.metaDesc ? <meta name="description" content={seo.metaDesc} /> : null}
-        {seo.opengraphTitle ? <meta property="og:title" content={seo.opengraphTitle} /> : null}
-        {seo.opengraphType ? <meta property="og:type" content={seo.opengraphType} /> : null}
-        {seo.opengraphPublisher ? <meta property="og:publisher" content={seo.opengraphPublisher} /> : null}
-        {seo.opengraphSiteName ? <meta property="og:site_name" content={seo.opengraphSiteName} /> : null}
-
-        {seo.opengraphAuthor ? <meta property="og:author" content={seo.opengraphAuthor} /> : null}
-        {seo.opengraphDescription ? <meta property="og:description" content={seo.opengraphDescription} /> : null}
-        {seo.opengraphModifiedTime ? <meta property="og:modified_time" content={seo.opengraphModifiedTime} /> : null}
-        {seo.opengraphPublishedTime ? <meta property="og:published_time" content={seo.opengraphPublishedTime} /> : null}
-        {seo.opengraphUrl ? <meta property="og:url" content={seo.opengraphUrl} /> : null}
+        <title>Sample Page - Greenherbs</title>
+        <meta property="og:title" content="Sample Page - Greenherbs" />
+        <meta property="og:type" content="article" />
+        <meta property="og:site_name" content="Greenherbs" />
+        <meta property="og:description" content="This is an example page. It&amp;#8217;s different from a blog post because it will stay in one place and will show up in your site navigation (in most themes). Most people start with an About page that introduces them to potential site visitors. It might say something like this: Hi there! I&amp;#8217;m a bike messenger [&amp;hellip;]" />
+        <meta property="og:url" content="https://api.dev.greenherbs.ru/sample-page/" />
       </Head>
       <div>
         Here is the seo content for this page, <pre>{JSON.stringify(data)}</pre>
