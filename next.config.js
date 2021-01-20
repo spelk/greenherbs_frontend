d3ebe34b640dc6957fa89f78ccb45c26ead4e954
alxbuts <alxbuts@gmail.com>
2021-01-20 12:45:46 +0200

    testing server-timing

--- next.config.js
+++ next.config.js
@@ -9,6 +9,19 @@ module.exports = {
         ? "https://apidev.greenherbs.ru/wp/graphql"
         : process.env.ENV === "staging"
         ? "https://apidev.greenherbs.ru/wp/graphql"
-        : "https://greenherbs.ru/wp/graphql",
+        : "https://api.greenherbs.ru/wp/graphql",
+  },
+  async headers() {
+    return [
+      {
+        source: '/about',
+        headers: [
+          {
+            key: 'Server-Timing',
+            value: 'db;dur=123;desc="Database", tmpl;dur=56;desc="Template processing"',
+          },
+        ],
+      },
+    ]
   },
 };
