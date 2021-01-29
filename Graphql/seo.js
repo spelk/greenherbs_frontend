ca0bb94fc9b70acbcdd12f48d0506a47ad8b330f
Timofey-Mileiko <mill.timofey@gmail.com>
2021-01-29 11:02:36 +0200

    separate graphql requests

--- Graphql/seo.js
+++ Graphql/seo.js
@@ -1,33 +1,33 @@
-// import { gql } from "@apollo/client";
+import { gql } from "@apollo/client";
 
-// export const GET_SEO = gql`
-// query getSeo {
-//   postBy(uri: "https://apidev.greenherbs.ru/${slug}" ) {
-//     seo {
-//       canonical
-//       cornerstone
-//       focuskw
-//       metaDesc
-//       metaKeywords
-//       metaRobotsNofollow
-//       metaRobotsNoindex
-//       opengraphAuthor
-//       opengraphDescription
-//       opengraphModifiedTime
-//       opengraphPublishedTime
-//       opengraphSiteName
-//       opengraphPublisher
-//       opengraphTitle
-//       opengraphType
-//       opengraphUrl
-//       twitterDescription
-//       title
-//       twitterTitle
-//       schema {
-//         articleType
-//         pageType
-//       }
-//     }
-//   }
-// }
-// `;
+export const GET_SEO = gql`
+    query getSeo ($page: String!) {
+      postBy(uri: $page ) {
+        seo {
+          canonical
+          cornerstone
+          focuskw
+          metaDesc
+          metaKeywords
+          metaRobotsNofollow
+          metaRobotsNoindex
+          opengraphAuthor
+          opengraphDescription
+          opengraphModifiedTime
+          opengraphPublishedTime
+          opengraphSiteName
+          opengraphPublisher
+          opengraphTitle
+          opengraphType
+          opengraphUrl
+          twitterDescription
+          title
+          twitterTitle
+          schema {
+            articleType
+            pageType
+          }
+        }
+      }
+    }
+`;
--- pages/[slug].js
+++ pages/[slug].js
@@ -11,43 +11,17 @@ function Slug() {
     query: { slug },
   } = useRouter();
 
-  // const { data } = useQuery(gql`
-  // query getSeo {
-  //   postBy(uri: "https://apidev.greenherbs.ru/${slug}" ) {
-  //     seo {
-  //       canonical
-  //       cornerstone
-  //       focuskw
-  //       metaDesc
-  //       metaKeywords
-  //       metaRobotsNofollow
-  //       metaRobotsNoindex
-  //       opengraphAuthor
-  //       opengraphDescription
-  //       opengraphModifiedTime
-  //       opengraphPublishedTime
-  //       opengraphSiteName
-  //       opengraphPublisher
-  //       opengraphTitle
-  //       opengraphType
-  //       opengraphUrl
-  //       twitterDescription
-  //       title
-  //       twitterTitle
-  //       schema {
-  //         articleType
-  //         pageType
-  //       }
-  //     }
-  //   }
-  // }
-  // `);
+  const { data } = useQuery(GET_SEO, {
+    variables: {
+      page: `https://apidev.greenherbs.ru/${slug}`
+    },
+  });
 
   return (
     <>
       <Menu />
       <div>
-        Here is the seo content for this page <h1>{slug}</h1>
+        Here is the seo content for this page <h1>{JSON.stringify(data)}</h1>
       </div>
     </>
   );
@@ -61,43 +35,16 @@ export async function getServerSideProps(context) {
   const apolloClient = initializeApollo();
 
   await apolloClient.query({
-    query: gql`
-    query getSeo {
-      postBy(uri: "https://apidev.greenherbs.ru/${slug}" ) {
-        seo {
-          canonical
-          cornerstone
-          focuskw
-          metaDesc
-          metaKeywords
-          metaRobotsNofollow
-          metaRobotsNoindex
-          opengraphAuthor
-          opengraphDescription
-          opengraphModifiedTime
-          opengraphPublishedTime
-          opengraphSiteName
-          opengraphPublisher
-          opengraphTitle
-          opengraphType
-          opengraphUrl
-          twitterDescription
-          title
-          twitterTitle
-          schema {
-            articleType
-            pageType
-          }
-        }
-      }
-    }
-    `,
+    query: GET_SEO,
+    variables: {
+      page: `https://apidev.greenherbs.ru/${slug}`
+    },
   });
 
   // Pass data to the page via props
   return {
     props: {
-      initialApolloState: {[slug]: apolloClient.cache.extract()},
+      initialApolloState: { [slug]: apolloClient.cache.extract() },
     },
   };
 }
